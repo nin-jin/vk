f7d1a992781cb7b4d7ac97bffc19481c4bd22040
nin-jin <nin-jin@ya.ru>
2020-09-14 10:33:48 +0300

    type fix

--- fund/store/store.ts
+++ fund/store/store.ts
@@ -2,8 +2,6 @@ namespace $ {
 
 	export class $vk_fund_store extends $mol_store<{
 		rises: readonly ReturnType< $vk_fund_rise['data'] >[]
-		payments: Record< string , string >
-		signs: Record< string , string >
 	}> {
 
 		@ $mol_mem
@@ -17,16 +15,6 @@ namespace $ {
 		rise( index : number ) {
 			return this.sub( 'rises' )
 				.sub( index , new $vk_fund_rise( this.value( 'rises' )[ index ] ?? {} ) )
-		}
-
-		@ $mol_mem
-		payments() {
-			return this.value( 'payments' )
-		}
-
-		@ $mol_mem
-		signs() {
-			return this.value( 'signs' )
 		}
 
 	}
